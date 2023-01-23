8e311531b45261859275c3a033eff6177c4e4dac
Roger Voss <Roger.Voss@tideworks.com>
2023-01-23 10:08:54 -0800

    Implemented locate_cfg_file() function to look for a config.ini file at different directory locations

--- main.cpp
+++ main.cpp
@@ -15,23 +15,17 @@ void __assert (const char *__assertion, const char *__file, int __line) __THROW 
 #endif
 
 using namespace logger;
-using std::string_view;
+using std::string_view_literals::operator ""sv;
 
 DECL_EXCEPTION(find_program_path)
 
 static int s_parent_thrd_pid = 0;
-static string_view s_progpath;
-static string_view s_progname;
+static const auto cfg_file_name = "config.ini"sv;
+static std::string_view s_progpath;
+static std::string_view s_progname;
 inline int get_parent_pid() { return s_parent_thrd_pid; }
 const char* progpath() { return s_progpath.data(); }
 const char* progname() { return s_progname.data(); }
-
-
-static std::string get_env_var(const char * const name) {
-  char * const val = getenv(name);
-  auto rtn_str( val != nullptr ? std::string(val) : std::string() );
-  return rtn_str;
-}
 
 enum class ACCEPT_ORDINAL : char {
   FIRST_FOUND = 0,
@@ -44,6 +38,11 @@ enum class ACCEPT_ORDINAL : char {
   LAST_FOUND = -1,
 };
 using AO = ACCEPT_ORDINAL;
+
+static std::string get_env_var(const char * const name) {
+  char * const val = getenv(name);
+  return val != nullptr ? std::string(val) : std::string();
+}
 
 static std::string find_program_path(const char * const prog, const char * const path_var_name) {
   const std::string path_env_var = get_env_var(path_var_name);
@@ -61,7 +60,7 @@ static std::string find_program_path(const char * const prog, const char * const
 
   while (path != nullptr) {
     log(LL::TRACE, "'%s'", path);
-    const string_view sv_path{path};
+    const std::string_view sv_path{path};
     const auto full_path = sv_path.ends_with(kPathSeparator) ?
         format2str("%s%s", path, prog) : format2str("%s%c%s", path, kPathSeparator, prog);
     log(LL::TRACE, "'%s'", full_path.c_str());
@@ -79,6 +78,37 @@ static std::string find_program_path(const char * const prog, const char * const
   throw find_program_path_exception(format2str(err_msg_fmt, prog, path_var_name));
 }
 
+static std::string locate_cfg_file() {
+  std::string dir;
+  std::string cfg_file_path;
+  struct stat statbuf{};
+  for (int check_exist = 3; ; check_exist--) {
+    switch(check_exist) {
+      case 3: // using HOME directory
+        dir = get_env_var("HOME");
+        if (dir.empty()) continue;
+        dir = path_concat(dir.c_str(), ".config");
+        dir = path_concat(dir.c_str(), "java-watchdog");
+        break;
+      case 2:
+        dir = "."; // using current working directory
+        break;
+      case 1:
+        dir = progpath(); // trying program-location directory
+      default:
+        return ""; // tried all possible dir locations but failed to find a config file
+    }
+    cfg_file_path = path_concat(dir.c_str(), cfg_file_name.data());
+    if (stat(cfg_file_path.c_str(), &statbuf) == -1 ||
+        ((statbuf.st_mode & S_IFMT) != S_IFREG || (statbuf.st_mode & S_IFMT) == S_IFLNK))
+    {
+      continue; // try a different dir path location
+    }
+    break; // found the config file so break out of loop
+  }
+  return cfg_file_path;
+}
+
 #pragma clang diagnostic push
 #pragma ide diagnostic ignored "LocalValueEscapesScope"
 #pragma ide diagnostic ignored "UnreachableCode"
@@ -89,24 +119,24 @@ static void one_time_init_main(int argc, const char *argv[])
   const char *malloced_tmpstr = nullptr;
 
   {
-    malloced_tmpstr = strdup(argv[0]);
+    malloced_tmpstr = strdup(argv[0]); // heap allocates the string storage
     if (malloced_tmpstr == nullptr) {
       __assert("strdup() could not duplicate program full path name on startup", __FILE__, __LINE__);
       _exit(EXIT_FAILURE); // will exit here if __assert() defined to no-op function
     }
-    s_progpath = malloced_tmpstr;
+    s_progpath = malloced_tmpstr; // static string_view variable takes ownership for program runtime duration
   }
 
   {
     malloced_tmpstr = [](const char* const path) -> const char* {
-      auto const dup_path = strdupa(path);
-      return strdup(basename(dup_path));
+      auto const dup_path = strdupa(path); // stack allocates the string storage (will go away on return)
+      return strdup(basename(dup_path));   // heap allocates the string storage
     }(progpath());
     if (malloced_tmpstr == nullptr) {
       __assert("strdup() could not duplicate program base name on startup", __FILE__, __LINE__);
       _exit(EXIT_FAILURE); // will exit here if __assert() defined to no-op function
     }
-    s_progname = malloced_tmpstr;
+    s_progname = malloced_tmpstr; // static string_view variable takes ownership for program runtime duration
   }
 
   logger::set_progname(progname());
@@ -118,76 +148,93 @@ static void one_time_init_main(int argc, const char *argv[])
 int main(int argc, const char *argv[]) {
   set_level(LL::DEBUG); // comment out this line to disable debug logging verbosity
   one_time_init_main(argc, argv);
+
+  // initialized to default settings
   LOGGING_LEVEL logging_level = LL::INFO;
   ACCEPT_ORDINAL accept_ordinal = AO::FIRST_FOUND;
 
-  auto prs_cfg_callback =
-      [&logging_level, &accept_ordinal](const char *section, const char *name, const char *value_cstr)
-  {
-    const auto to_lower = [](std::string &str) {
-      transform(str.begin(), str.end(), str.begin(), ::tolower);
-    };
+  const auto cfg_file_path = locate_cfg_file();
+  if (!cfg_file_path.empty()) {
+    auto prs_cfg_callback =
+        [&logging_level, &accept_ordinal](const char *section, const char *name, const char *value_cstr)
+        {
+          const auto to_lower = [](std::string &str) {
+            transform(str.begin(), str.end(), str.begin(), ::tolower);
+          };
 
-    std::string s_section{section};
-    to_lower(s_section);
-    if (s_section.compare("settings")) {
-      std::string s_name{name};
-      to_lower(s_name);
-      std::string s_value{value_cstr};
-      to_lower(s_value);
-      if (s_name.compare("logging_level")) {
-        if (s_value.compare("trace")) {
-          logging_level = LL::TRACE;
-        } else if (s_value.compare("debug")) {
-          logging_level = LL::DEBUG;
-        } else if (s_value.compare("info")) {
-          logging_level = LL::INFO;
-        } else if (s_value.compare("warn")) {
-          logging_level = LL::WARN;
-        } else if (s_value.compare("err")) {
-          logging_level = LL::ERR;
-        } else {
-          logging_level = LL::INFO;
-        }
-      } else if (s_name.compare("accept_ordinal")) {
-        if (s_value.compare("first_found")) {
-          accept_ordinal = AO::FIRST_FOUND;
-        } else if (s_value.compare("last_found")) {
-          accept_ordinal = AO::LAST_FOUND;
-        } else if (s_value.compare("second_found")) {
-          accept_ordinal = AO::SECOND_FOUND;
-        } else if (s_value.compare("third_found")) {
-          accept_ordinal = AO::THIRD_FOUND;
-        } else if (s_value.compare("fourth_found")) {
-          accept_ordinal = AO::FOURTH_FOUND;
-        } else if (s_value.compare("fifth_found")) {
-          accept_ordinal = AO::FIFTH_FOUND;
-        } else if (s_value.compare("sixth_found")) {
-          accept_ordinal = AO::SIXTH_FOUND;
-        } else if (s_value.compare("seventh_found")) {
-          accept_ordinal = AO::SEVENTH_FOUND;
-        } else {
-          log(LL::WARN, "unrecognized settings section %s value \"%s\" ignored", name, value_cstr);
-          return 1;
-        }
-      } else {
-        log(LL::WARN, "unrecognized settings section name %s ignored", name);
-        return 1;
+          std::string s_section{section};
+          to_lower(s_section);
+          if (s_section.compare("settings")) {
+            std::string s_name{name};
+            to_lower(s_name);
+            std::string s_value{value_cstr};
+            to_lower(s_value);
+            if (s_name.compare("logging_level")) {
+              if (s_value.compare("trace")) {
+                logging_level = LL::TRACE;
+              } else if (s_value.compare("debug")) {
+                logging_level = LL::DEBUG;
+              } else if (s_value.compare("info")) {
+                logging_level = LL::INFO;
+              } else if (s_value.compare("warn")) {
+                logging_level = LL::WARN;
+              } else if (s_value.compare("err")) {
+                logging_level = LL::ERR;
+              } else {
+                logging_level = LL::INFO;
+                log(LL::WARN, "logging level '%s' not recognized - defaulting to INFO", value_cstr);
+              }
+            } else if (s_name.compare("accept_ordinal")) {
+              if (s_value.compare("first_found")) {
+                accept_ordinal = AO::FIRST_FOUND;
+              } else if (s_value.compare("last_found")) {
+                accept_ordinal = AO::LAST_FOUND;
+              } else if (s_value.compare("second_found")) {
+                accept_ordinal = AO::SECOND_FOUND;
+              } else if (s_value.compare("third_found")) {
+                accept_ordinal = AO::THIRD_FOUND;
+              } else if (s_value.compare("fourth_found")) {
+                accept_ordinal = AO::FOURTH_FOUND;
+              } else if (s_value.compare("fifth_found")) {
+                accept_ordinal = AO::FIFTH_FOUND;
+              } else if (s_value.compare("sixth_found")) {
+                accept_ordinal = AO::SIXTH_FOUND;
+              } else if (s_value.compare("seventh_found")) {
+                accept_ordinal = AO::SEVENTH_FOUND;
+              } else {
+                accept_ordinal = AO::FIRST_FOUND;
+                log(LL::WARN, "unrecognized settings section %s value '%s' - defaulting to FIRST_FOUND",
+                    name, value_cstr);
+              }
+            } else {
+              log(LL::WARN, "unrecognized settings section name '%s' ignored", name);
+            }
+          } else {
+            log(LL::WARN, "unrecognized config section '%s' ignored", section);
+          }
+          return 0;
+        };
+
+    try {
+      if (!process_config(cfg_file_path.c_str(), prs_cfg_callback)) {
+        // reset to defaults
+        logging_level = LL::INFO;
+        accept_ordinal = AO::FIRST_FOUND;
       }
-    } else {
-      log(LL::WARN, "unrecognized config section %s ignored", section);
-      return 1;
+    } catch(const process_cfg_exception &ex) {
+      // reset to defaults
+      logging_level = LL::INFO;
+      accept_ordinal = AO::FIRST_FOUND;
+      log(LL::WARN, "failed processing config file - using default settings:\n\t%s:%s", ex.name(), ex.what());
     }
-    return 0;
-  };
-  process_config(progpath(), "config.ini", prs_cfg_callback);
+  }
 
   try {
     // determine fully qualified path to the program
     const auto java_prog_path = find_program_path("java", "PATH");
     log(LL::DEBUG, "Java launcher program: \"%s\"", java_prog_path.c_str());
   } catch(const find_program_path_exception &ex) {
-    log(LL::ERR, "could not locate a valid Java launcher program - %s: %s", ex.name(), ex.what());
+    log(LL::ERR, "could not locate a Java launcher program:\n\t%s: %s", ex.name(), ex.what());
   }
   return 0;
 }
